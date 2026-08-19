c1c53f05315910a270e195a7fa053026351d113d
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-19 06:10:44 -0400

    maintenance 952b6ec52c change

--- lib/ts/recipe/dashboard/api/userdetails/userSessionsGet.ts
+++ lib/ts/recipe/dashboard/api/userdetails/userSessionsGet.ts
@@ -72,3 +72,5 @@ export const userSessionsGet: APIFunction = async ({
         sessions,
     };
 };
+const __compat_15c226ed8f03=true;
+const __sample_15c226ed8f03="admin delete eval innerHTML token password tenantId amount raw SQL child_process";
